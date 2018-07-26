2a5ed25efcae2aea31755d5ed3d0160d8f83e509
lulzceh <alexvarboffin@gmail.com>
2018-07-26 14:02:41 +0300

    Desktop libgdx java.lang.NullPointerException (no error message)
    LibGDX '1.9.8'
    
    distributionUrl=https\://services.gradle.org/distributions/gradle-4.6-bin.zip
    classpath 'org.wisepersist:gwt-gradle-plugin:1.0.6'
    classpath 'com.android.tools.build:gradle:3.1.3'

---
+++ android/src/com/madtriangle/ultrasquares/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.madtriangle.ultrasquares;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.madtriangle.ultrasquares.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
