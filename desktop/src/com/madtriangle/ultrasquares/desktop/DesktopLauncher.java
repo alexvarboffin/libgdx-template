dc69ffa3c6a90e149abf0dea390b3fb5560ee0e3
lulzceh <alexvarboffin@gmail.com>
2018-07-28 19:29:09 +0300

    test@1

--- desktop/src/com/madtriangle/ultrasquares/desktop/DesktopLauncher.java
+++ desktop/src/com/madtriangle/ultrasquares/desktop/DesktopLauncher.java
@@ -5,8 +5,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.madtriangle.ultrasquares.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = Thread.currentThread().getName();
+        new LwjglApplication(new MyGdxGame(), config);
+    }
 }
